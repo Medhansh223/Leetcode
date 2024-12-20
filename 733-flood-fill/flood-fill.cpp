91c4091e5e938340fa66a8a9c47b6f0c9841753e
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-20 23:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (19.22%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    void dfs(int sr, int sc, vector<vector<int>>& ans, vector<vector<int>>& image, int color, int row[], int col[], int initial)
+    void bfs(int sr, int sc, vector<vector<int>>& ans, vector<vector<int>>& image, int color, int row[], int col[], int initial)
     {
         ans[sr][sc]=color;
         int n=image.size();
         int m=image[0].size();
-        for(int i=0;i<4;i++)
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        while(!q.empty())
         {
-            int nrow=sr+row[i];
-            int ncol=sc+col[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==initial && ans[nrow][ncol]!=color)
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
             {
-                dfs(nrow,ncol,ans,image,color,row,col,initial);
+                int nrow=r+row[i];
+                int ncol=c+col[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==initial && ans[nrow][ncol]!=color)
+                {
+                    q.push({nrow,ncol});
+                    ans[nrow][ncol]=color;
+                }
             }
         }
     }
@@ -20,7 +29,7 @@ public:
         vector<vector<int>>ans=image;
         int row[]={-1,0,+1,0};
         int col[]={0,+1,0,-1};
-        dfs(sr,sc,ans,image,color,row,col,initial);
+        bfs(sr,sc,ans,image,color,row,col,initial);
         return ans;
     }
 };

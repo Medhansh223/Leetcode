afa652dae47ce4d43fe609432ebb5647c756532a
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-23 23:40:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (21.80%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& ans, vector<vector<int>>& image, int sr, int sc, int color, int& initial)
+    void dfs(vector<vector<int>>& ans, vector<vector<int>>& image, int sr, int sc, int color, int& initial)
     {
         int n=image.size();
         int m=image[0].size();
         ans[sr][sc]=color;
         vector<int>drow={-1,0,+1,0};
         vector<int>dcol={0,+1,0,-1};
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty())
+        for(int i=0;i<4;i++)
         {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
-            {
-                int row=r+drow[i];
-                int col=c+dcol[i];
-                if(row>=0 && row<n && col>=0 && col<m && image[row][col]==initial && ans[row][col]!=color)
-                {
-                    q.push({row,col});
-                    ans[row][col]=color;
-                }
+            int row=sr+drow[i];
+            int col=sc+dcol[i];
+            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==initial && ans[row][col]!=color)
+            {                   
+                dfs(ans,image,row,col,color,initial);
             }
         }
     }
@@ -31,7 +22,7 @@ public:
         int m=image[0].size();
         int initial=image[sr][sc];
         vector<vector<int>>ans=image;
-        bfs(ans,image,sr,sc,color,initial);
+        dfs(ans,image,sr,sc,color,initial);
         return ans;
     }
 };

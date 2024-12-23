68f87dd1543bcacdd1ca67326bdc07b7e27bfb99
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-23 22:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (15.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
+    void bfs(int& time, queue<pair<pair<int,int>,int>>& q, vector<vector<int>>& visited, vector<vector<int>>& grid)
+    {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m));
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==2)
-                {
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                }
-            }
-        }
-        int time=0;
-        int row[]={-1,0,+1,0};
-        int col[]={0,+1,0,-1};
+        vector<int>drow={-1,0,+1,0};
+        vector<int>dcol={0,+1,0,-1};
         while(!q.empty())
         {
             int r=q.front().first.first;
@@ -28,20 +15,39 @@ public:
             q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=r+row[i];
-                int ncol=c+col[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2)
+                int row=r+drow[i];
+                int col=c+dcol[i];
+                if(row>=0 && row<n && col>=0 && col<m && grid[row][col]==1 && visited[row][col]!=2)
                 {
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
+                    q.push({{row,col},t+1});
+                    visited[row][col]=2;
                 }
             }
         }
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>visited(n,vector<int>(m));
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(vis[i][j]!=2 && grid[i][j]==1)
+                if(grid[i][j]==2 && visited[i][j]!=2)
+                {   
+                    q.push({{i,j},0});
+                    visited[i][j]=2;
+                }
+            }
+        }
+        int time=0;
+        bfs(time,q,visited,grid);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1 && visited[i][j]!=2)
                 {
                     return -1;
                 }

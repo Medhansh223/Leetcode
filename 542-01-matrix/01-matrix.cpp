163fcb7fae25354e6cd7a587add8071c8bc65b1d
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-24 11:56:12 +0530

    Time: 31 ms (26.91%) | Memory: 39.1 MB (16.56%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void bfs(vector<int>&drow, vector<int>&dcol, vector<vector<int>>&visited, vector<vector<int>>&distance, vector<vector<int>>&mat, queue<pair<pair<int,int>,int>>&q)
+    void bfs(vector<vector<int>>& mat, vector<vector<int>>& visited, vector<vector<int>>& distance, queue<pair<pair<int,int>,int>>& q)
     {
         int n=mat.size();
         int m=mat[0].size();
+        vector<int>drow={-1,0,+1,0};
+        vector<int>dcol={0,+1,0,-1};
         while(!q.empty())
         {
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int steps=q.front().second;
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int s=q.front().second;
             q.pop();
-            distance[row][col]=steps;
+            distance[r][c]=s;
             for(int i=0;i<4;i++)
             {
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0)
                 {
+                    q.push({{nrow,ncol},s+1});
                     visited[nrow][ncol]=1;
-                    q.push({{nrow,ncol},steps+1});
                 }
             }
         }
@@ -26,23 +28,21 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        vector<vector<int>>distance(n,vector<int>(m,0));
+        vector<vector<int>>visited(n,vector<int>(m));
+        vector<vector<int>>distance(n,vector<int>(m));
         queue<pair<pair<int,int>,int>>q;
-        vector<int>drow={-1,0,+1,0};
-        vector<int>dcol={0,+1,0,-1};
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(mat[i][j]==0)
+                if(mat[i][j]==0 && visited[i][j]!=1)
                 {
                     q.push({{i,j},0});
                     visited[i][j]=1;
                 }
             }
         }
-        bfs(drow,dcol,visited,distance,mat,q);
+        bfs(mat,visited,distance,q);
         return distance;
     }
 };

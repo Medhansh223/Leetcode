f29834a9a1ce9feab0dd998f976f0eebfa4e3d6d
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-25 11:11:27 +0530

    Time: 209 ms (9.11%) | Memory: 58.9 MB (5.01%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    void bfs(queue<pair<int,int>>& q, vector<vector<int>>& visited, vector<vector<int>>& grid)
+    void dfs(int row, int col, queue<pair<int,int>>& q, vector<vector<int>>& visited, vector<vector<int>>& grid)
     {
         int n=grid.size();
         int m=grid[0].size();
         vector<int>drow={-1,0,+1,0};
         vector<int>dcol={0,+1,0,-1};
-        while(!q.empty())
+        visited[row][col]=1;
+        for(int i=0;i<4;i++)
         {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]!=1)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]!=1)
-                {
-                    visited[nrow][ncol]=1;
-                    q.push({nrow,ncol});
-                }
+                dfs(nrow,ncol,q,visited,grid);
             }
         }
     }
@@ -36,13 +30,11 @@ public:
                 {
                     if(grid[i][j]==1 && visited[i][j]!=1)
                     {
-                        q.push({i,j});
-                        visited[i][j]=1;
+                        dfs(i,j,q,visited,grid);
                     }
                 }
             }
         }
-        bfs(q,visited,grid);
         int count=0;
         for(int i=0;i<n;i++)
         {

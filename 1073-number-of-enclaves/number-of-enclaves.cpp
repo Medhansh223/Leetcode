3e737111e02c210cfb6bb7e2bdb9aa9cfe604ca8
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-25 11:09:34 +0530

    Time: 7 ms (81.46%) | Memory: 31.5 MB (75.33%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>>& visited, vector<vector<int>>& grid)
+    void bfs(queue<pair<int,int>>& q, vector<vector<int>>& visited, vector<vector<int>>& grid)
     {
         int n=grid.size();
         int m=grid[0].size();
-        visited[row][col]=1;
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
-        for(int i=0;i<4;i++)
+        vector<int>drow={-1,0,+1,0};
+        vector<int>dcol={0,+1,0,-1};
+        while(!q.empty())
         {
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]!=1)
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
             {
-                dfs(nrow,ncol,visited,grid);
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]!=1)
+                {
+                    visited[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
             }
         }
     }
-
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
+        vector<vector<int>>visited(n,vector<int>(m));
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -30,22 +36,24 @@ public:
                 {
                     if(grid[i][j]==1 && visited[i][j]!=1)
                     {
-                        dfs(i,j,visited,grid);
+                        q.push({i,j});
+                        visited[i][j]=1;
                     }
                 }
             }
         }
-        int enclaves=0;
+        bfs(q,visited,grid);
+        int count=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1 && visited[i][j]!=1)
                 {
-                    enclaves++;
+                    count++;
                 }
             }
         }
-        return enclaves;
+        return count;
     }
 };

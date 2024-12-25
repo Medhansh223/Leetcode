719f64f386f8113590e04f37a8f5de2708cefbac
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-12-25 11:01:35 +0530

    Time: 1 ms (35.62%) | Memory: 16 MB (14.49%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    void bfs(queue<pair<int,int>>& q, vector<vector<int>>& visited, vector<vector<char>>& board)
+    void dfs(int row, int col, queue<pair<int,int>>& q, vector<vector<int>>& visited, vector<vector<char>>& board)
     {
         int n=board.size();
         int m=board[0].size();
         vector<int>drow={-1,0,+1,0};
         vector<int>dcol={0,+1,0,-1};
-        while(!q.empty())
+        visited[row][col]=1;
+        for(int i=0;i<4;i++)
         {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]=='O' && visited[nrow][ncol]!=1)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]=='O' && visited[nrow][ncol]!=1)
-                {
-                    q.push({nrow,ncol});
-                    visited[nrow][ncol]=1;
-                }
+                dfs(nrow,ncol,q,visited,board);
             }
         }
     }
@@ -36,13 +30,11 @@ public:
                 {
                     if(board[i][j]=='O' && visited[i][j]!=1)
                     {
-                        q.push({i,j});
-                        visited[i][j]=1;
+                        dfs(i,j,q,visited,board);
                     }
                 }
             }
         }
-        bfs(q,visited,board);
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)

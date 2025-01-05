ad2416d00ec45b5e999b08b80eb939f3118d1653
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-01-05 20:12:19 +0530

    Time: 25 ms (39.13%) | Memory: 47 MB (40.96%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,9 +1,10 @@
 class DisjointSet{
-    vector<int> parent,rank,size;
+    vector<int>parent,rank,size;
 public:
-    DisjointSet(int n){
-        rank.resize(n+1,0);
+    DisjointSet(int n)
+    {
         parent.resize(n+1,0);
+        rank.resize(n+1,0);
         size.resize(n+1,0);
         for(int i=0;i<n;i++)
         {
@@ -11,40 +12,41 @@ public:
             size[i]=1;
         }
     }
-    int find(int X){
-        if(X==parent[X])
+    int find(int n)
+    {
+        if(n==parent[n])
         {
-            return X;
+            return n;
         }
-        return parent[X]=find(parent[X]);
+        return parent[n]=find(parent[n]);
     }
-    void unionRank(int X,int Z) // union by rank
+    void UnionRank(int u, int v)
     {
-    	int ulp_u=parent[X];
-    	int ulp_v=parent[Z];
-    	if(ulp_u==ulp_v)
-    	{
-    	    return;
-    	}
-    	if(rank[ulp_u]<rank[ulp_v])
-    	{
-    	    parent[ulp_u]=ulp_v;
-    	}
-    	else if(rank[ulp_v]<rank[ulp_u])
-    	{
-    	    parent[ulp_v]=ulp_u;
-    	}
-    	else
-    	{
-    	    parent[ulp_v]=ulp_u;
-    	    rank[ulp_u]++;
-    	}
+        int ulp_u=parent[u];
+        int ulp_v=parent[v];
+        if(ulp_u == ulp_v)
+        {
+            return;
+        }
+        if(rank[ulp_u]<rank[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u])
+        {
+            parent[ulp_v]=ulp_u;
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
     }
-    void unionSize(int X,int Z) // union by size
+    void UnionSize(int u, int v)
     {
-        int ulp_u=parent[X];
-        int ulp_v=parent[Z];
-        if(ulp_u==ulp_v)
+        int ulp_u=parent[u];
+        int ulp_v=parent[v];
+        if(ulp_u == ulp_v)
         {
             return;
         }
@@ -64,19 +66,19 @@ public:
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
+        int edges=0;
         DisjointSet ds(n);
-        int extraedges=0;
         for(auto it:connections)
         {
             int u=it[0];
             int v=it[1];
-            if(ds.find(u)==ds.find(v))
+            if(ds.find(u) == ds.find(v))
             {
-                extraedges++;
+                edges++;
             }
             else
             {
-                ds.unionSize(u,v);
+                ds.UnionSize(u,v);
             }
         }
         int components=0;
@@ -88,7 +90,7 @@ public:
             }
         }
         int ans=components-1;
-        if(extraedges>=ans)
+        if(edges>=ans)
         {
             return ans;
         }

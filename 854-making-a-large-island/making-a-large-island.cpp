304b35b8003055ff0ff337e5dde03a81e2f7983a
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-01-07 01:24:49 +0530

    Time: 195 ms (61.39%) | Memory: 115.5 MB (65.25%) - LeetSync

---
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -0,0 +1,127 @@
+class DisjointSet{
+public:
+    vector<int>parent,rank,size;
+    DisjointSet(int n)
+    {
+        parent.resize(n+1,0);
+        rank.resize(n+1,0);
+        size.resize(n+1,0);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    int find(int n)
+    {
+        if(n==parent[n])
+        {
+            return n;
+        }
+        return parent[n]=find(parent[n]);
+    }
+    void UnionRank(int u, int v)
+    {
+        int ulp_u=find(u);
+        int ulp_v=find(v);
+        if(ulp_u == ulp_v)
+        {
+            return;
+        }
+        if(rank[ulp_u]<rank[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u])
+        {
+            parent[ulp_v]=ulp_u;
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void UnionSize(int u, int v)
+    {
+        int ulp_u=find(u);
+        int ulp_v=find(v);
+        if(ulp_u == ulp_v)
+        {
+            return;
+        }
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]=size[ulp_v]+size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]=size[ulp_u]+size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        DisjointSet ds(n*n);
+        vector<int>drow={-1,0,+1,0};
+        vector<int>dcol={0,+1,0,-1};
+        for(int row=0;row<n;row++)
+        {
+            for(int col=0;col<n;col++)
+            {
+                if(grid[row][col]==0)
+                {
+                    continue;
+                }
+                for(int k=0;k<4;k++)
+                {
+                    int nrow=row+drow[k];
+                    int ncol=col+dcol[k];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==1)
+                    {
+                        int node=(row*n)+col;
+                        int adjacent=(nrow*n)+ncol;
+                        ds.UnionSize(node, adjacent);
+                    }
+                }
+            }   
+        }
+        int maxi=0;
+        for(int row=0;row<n;row++)
+        {
+            for(int col=0;col<n;col++)
+            {
+                set<int>st;
+                if(grid[row][col]==1)
+                {
+                    continue;
+                }
+                for(int k=0;k<4;k++)
+                {
+                    int nrow=row+drow[k];
+                    int ncol=col+dcol[k];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==1)
+                    {
+                        st.insert(ds.find((nrow*n)+ncol));
+                    }
+                }
+                int totalsize=0;
+                for(auto it:st)
+                {
+                    totalsize=totalsize+ds.size[it];
+                }
+                maxi=max(maxi,totalsize+1);
+            }   
+        }
+        for(int i=0;i<n*n;i++)
+        {
+            maxi=max(maxi,ds.size[ds.find(i)]);
+        }
+        return maxi;
+    }
+};

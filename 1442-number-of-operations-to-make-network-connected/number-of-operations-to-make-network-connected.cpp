0e9885d8a18128422b198fdb45cb3e1cee596163
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-01-07 16:09:16 +0530

    Time: 19 ms (61.67%) | Memory: 47.2 MB (36.34%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,12 +1,12 @@
 class DisjointSet{
-    vector<int>parent,rank,size;
 public:
+    vector<int>parent,rank,size;
     DisjointSet(int n)
     {
         parent.resize(n+1,0);
         rank.resize(n+1,0);
         size.resize(n+1,0);
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
         {
             parent[i]=i;
             size[i]=1;

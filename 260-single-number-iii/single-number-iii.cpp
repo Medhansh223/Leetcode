e5fcc42024f186b92cd2aeaa339500527c8b94be
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-01-20 00:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (77.92%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        long long xor1=0;
+        for(int i=0;i<n;i++)
+        {
+            xor1=xor1^nums[i];
+        }
+        long long right_bit = xor1 ^ (xor1 & (xor1-1));
+        int b1=0;
+        int b2=0;
+        for(int i=0;i<n;i++)
+        {
+            if((nums[i] & right_bit)==0)
+            {
+                b1=b1^nums[i];
+            }
+            else
+            {
+                b2=b2^nums[i];
+            }
+        }
+        return {b1,b2};
+    }
+};

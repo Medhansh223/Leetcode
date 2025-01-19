acd24352dcec4798afa201509fb121fd09fad326
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-01-19 22:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.82%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -6,26 +6,26 @@ public:
             return 1;
         }
         bool sign=true;
-        if(dividend>0 && divisor<0)
+        if(dividend>=0 && divisor<0)
         {
             sign=false;
         }
-        if(dividend<0 && divisor>0)
+        if(dividend<=0 && divisor>0)
         {
             sign=false;
         }
-        long n = abs((long)dividend);
-        long d = abs((long)divisor);
+        long n = abs(long(dividend));
+        long d = abs(long(divisor));
         int ans=0;
         while(n>=d)
         {
             int count=0;
-            while(n >= (d << (count+1)))
+            while(n>=(d<<(count+1)))
             {
                 count++;
             }
-            ans = ans + (1<<count);
-            n = n - (d<<count);
+            ans=ans+(1<<count);
+            n=n-(d<<count);
         }
         if(ans==(1<<31) && sign==true)
         {

96ccc1961b7b8de06a47e121b73472a3ed51db70
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-02-01 12:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.94%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -34,11 +34,31 @@ public:
         }
         return dp[n-1];
     }
+    int optimization(vector<int>& nums, int n)
+    {
+        int curr;
+        int prev1=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++)
+        {
+            int pick=nums[i];
+            if(i>=2)
+            {
+                pick=pick+prev2;
+            }
+            int non_pick=prev1;
+            curr=max(pick,non_pick);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
         // int ans1 = memoization(nums,dp,n-1);
-        int ans2 = tabulation(nums,dp,n);
-        return ans2;
+        // int ans2 = tabulation(nums,dp,n);
+        int ans3 = optimization(nums,n);
+        return ans3;
     }
 };

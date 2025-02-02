94f63aa94bf4086e9541c71d0f7fd4dcc582154e
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-02-02 13:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (89.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -56,12 +56,50 @@ public:
         }
         return dp[m-1][n-1];
     }
+
+    int optimization(int m, int n, vector<int>& prev, vector<vector<int>>& obstacleGrid)
+    {
+        for(int i=0;i<m;i++)
+        {
+            vector<int>temp(n,-1);
+            for(int j=0;j<n;j++)
+            {
+                if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+                {
+                    temp[j]=0;
+                }
+                else if(i==0 && j==0)
+                {
+                    temp[j]=1;
+                }
+                else
+                {
+                    int up=0;
+                    int left=0;
+                    if(i>0)
+                    {
+                        up=prev[j];
+                    }
+                    if(j>0)
+                    {
+                        left=temp[j-1];
+                    }
+                    temp[j]=up+left;
+                }
+            }
+            prev=temp;
+        }
+        return prev[n-1];
+    }
+
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        int ans1 = memoization(m-1,n-1,dp,obstacleGrid);
-        int ans2 = tabulation(m,n,dp,obstacleGrid);
-        return ans2;
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // int ans1 = memoization(m-1,n-1,dp,obstacleGrid);
+        // int ans2 = tabulation(m,n,dp,obstacleGrid);
+        vector<int>dp(n,-1);
+        int ans3 = optimization(m,n,dp,obstacleGrid);
+        return ans3;
     }
 };

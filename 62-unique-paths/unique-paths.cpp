752e8471ed14599662d3cc0c52a0812cbec88df3
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-02-02 12:34:09 +0530

    Time: 2 ms (18.58%) | Memory: 9.2 MB (56.76%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -29,19 +29,21 @@ public:
                 if(i==0 && j==0)
                 {
                     dp[i][j]=1;
-                    continue;
                 }
-                int up=0;
-                int left=0;
-                if(i>0)
+                else
                 {
-                    up=dp[i-1][j];
+                    int up=0;
+                    int left=0;
+                    if(i>0)
+                    {
+                        up=dp[i-1][j];
+                    }
+                    if(j>0)
+                    {
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=up+left;
                 }
-                if(j>0)
-                {
-                    left=dp[i][j-1];
-                }
-                dp[i][j]=up+left;
             }
         }
         return dp[m-1][n-1];

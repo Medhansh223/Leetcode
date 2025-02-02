f47b810fe0cfc61eea1ab99cbba59519dbbc3ee1
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-02-02 13:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (55.17%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -23,11 +23,45 @@ public:
         dp[i][j] = up+left;
         return dp[i][j];
     }
+
+    int tabulation(int m, int n, vector<vector<int>>& dp, vector<vector<int>>& obstacleGrid)
+    {
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                }
+                else if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                }
+                else
+                {
+                    int up=0;
+                    int left=0;
+                    if(i>0)
+                    {
+                        up=dp[i-1][j];
+                    }
+                    if(j>0)
+                    {
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
         int ans1 = memoization(m-1,n-1,dp,obstacleGrid);
-        return ans1;
+        int ans2 = tabulation(m,n,dp,obstacleGrid);
+        return ans2;
     }
 };

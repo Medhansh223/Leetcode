c8fdf913a66137d37048210af14e76f93e1b6759
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-02-18 11:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.96%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long power(long long base, long long count, int mod) {
+        long long result = 1;
+        while (count > 0) {
+            if(count % 2 == 1) 
+            {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            count = count / 2;
+        }
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        int mod = 1e9 + 7;
+        long long countevenplaces = (n+1)/2;
+        long long countoddplaces = n/2;
+        long long power5 = power(5, countevenplaces, mod);
+        long long power4 = power(4, countoddplaces, mod);
+        return (power5 * power4 % mod);
+    }
+};

0dbb10ca961373caef5494e2cddb161302c524ec
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-03-03 13:46:00 +0530

    Time: 7 ms (59.22%) | Memory: 127.6 MB (87.50%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,40 +2,30 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        int countless = 0;
-        int countequal = 0;
-        for(int i=0;i<n;i++)
+        vector<int>ans(n);
+        int i = 0;
+        int j = n-1;
+        int k = 0;
+        int l = n-1;
+        while(i < n && j >= 0)
         {
             if(nums[i] < pivot)
             {
-                countless++;
-            }
-            if(nums[i] == pivot)
-            {
-                countequal++;
-            }
-        }
-        vector<int>ans(n);
-        int i = 0;
-        int j = countless;
-        int k = countless + countequal;
-        for(auto it:nums)
-        {
-            if(it < pivot)
-            {
-                ans[i] = it;
-                i++;
-            }
-            if(it == pivot)
-            {
-                ans[j] = it;
-                j++;
-            }
-            if(it > pivot)
-            {
-                ans[k] = it;
+                ans[k] = nums[i];
                 k++;
             }
+            if(nums[j] > pivot)
+            {
+                ans[l] = nums[j];
+                l--;
+            }
+            i++;
+            j--;
+        }
+        while(k <= l)
+        {
+            ans[k] = pivot;
+            k++;
         }
         return ans;
     }

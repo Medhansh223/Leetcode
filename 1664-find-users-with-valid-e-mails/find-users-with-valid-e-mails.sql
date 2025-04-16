6140f94db167db2b304d2afa0a43212a9e7ea3dd
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-16 11:44:20 +0530

    Time: 947 ms (59.84%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 select * from Users
-where mail REGEXP '^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode[.]com$'
+where mail REGEXP '^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode[.]*com$'

d64d42c5a7b9894bce49095c0d9348743b549246
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-17 18:22:56 +0530

    Time: 3 ms (8.07%) | Memory: 11.1 MB (9.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,92 +1,84 @@
 class Solution {
 public:
-    int memoization(vector<int>& nums, vector<int>& dp, int n)
+    int memo(vector<int>&dp,int n,vector<int>&nums)
     {
         if(n==0)
         {
             return nums[n];
         }
-        if(n<0)
-        {
-            return 0;
-        }
         if(dp[n]!=-1)
         {
             return dp[n];
         }
-        int pick = nums[n] + memoization(nums,dp,n-2);
-        int not_pick = 0 + memoization(nums,dp,n-1);
-        dp[n] = max(pick, not_pick);
+        int pick=nums[n];
+        if(n-2>=0)
+        {
+            pick=pick+memo(dp,n-2,nums);
+        }
+        int nonpick=memo(dp,n-1,nums);
+        dp[n]=max(pick,nonpick);
         return dp[n];
     }
-    int tabulation(vector<int>& nums, vector<int>& dp, int n)
+    int tabu(vector<int>&dp,int n,vector<int>&nums)
     {
         dp[0]=nums[0];
         for(int i=1;i<n;i++)
         {
             int pick=nums[i];
-            if(i>=2)
+            if(i-2>=0)
             {
                 pick=pick+dp[i-2];
             }
-            int non_pick=dp[i-1];
-            dp[i]=max(pick,non_pick);
+            int nonpick=dp[i-1];
+            dp[i]=max(pick,nonpick);
         }
         return dp[n-1];
     }
-    int optimization(vector<int>& nums)
+    int space(int n,vector<int>&nums)
     {
-        int n=nums.size();
-        int curr;
         int prev1=nums[0];
         int prev2=0;
+        int curr;
         for(int i=1;i<n;i++)
         {
             int pick=nums[i];
-            if(i>=2)
+            if(i-2>=0)
             {
                 pick=pick+prev2;
             }
-            int non_pick=prev1;
-            curr=max(pick,non_pick);
+            int nonpick=prev1;
+            curr=max(pick,nonpick);
             prev2=prev1;
             prev1=curr;
         }
         return prev1;
     }
-
     int robo(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        int ans1 = memoization(nums,dp,n-1);
-        int ans2 = tabulation(nums,dp,n);
-        int ans3 = optimization(nums);
-        return ans3;
+        // return memo(dp,n-1,nums);
+        // return tabu(dp,n,nums);
+        return space(n,nums);
     }
-
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)
         {
             return nums[0];
         }
-        vector<int>dp(n,-1);
-        vector<int>firstnumber;
-        vector<int>lastnumber;
+        vector<int>left;
+        vector<int>right;
         for(int i=0;i<n;i++)
         {
             if(i!=0)
             {
-                firstnumber.push_back(nums[i]);
+                left.push_back(nums[i]);
             }
-            if(i!=(n-1))
+            if(i!=n-1)
             {
-                lastnumber.push_back(nums[i]);
+                right.push_back(nums[i]);
             }
         }
-        // int ans1 = memoization(nums,dp,n-1);
-        // int ans2 = tabulation(nums,dp,n);
-        int ans3 = max(robo(firstnumber), robo(lastnumber));
-        return ans3;
+        return max(robo(left),robo(right));
     }
 };

daf9eeca1ab8123334e08d540c21d30a716fad73
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-18 12:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (61.36%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -16,9 +16,33 @@ public:
         dp[i][j]=min(down,dj);
         return dp[i][j];
     }
+    int tabu(vector<vector<int>>&dp,vector<vector<int>>&triangle)
+    {
+        int n=triangle.size();
+        dp[0][0]=triangle[0][0];
+        int mini=INT_MAX;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                int up = INT_MAX, ld = INT_MAX;
+
+                if (j < i) up = triangle[i][j] + dp[i - 1][j]; // from directly above
+                if (j > 0) ld = triangle[i][j] + dp[i - 1][j - 1]; // from left diagonal);
+                dp[i][j]=min(up,ld);
+            }
+        }
+        int s=dp[n-1].size();
+        for(int i=0;i<s;i++)
+        {   
+            mini=min(mini,dp[n-1][i]);
+        }
+        return mini;
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return memo(0,0,dp,triangle);
+        // return memo(0,0,dp,triangle);
+        return tabu(dp,triangle);
     }
 };

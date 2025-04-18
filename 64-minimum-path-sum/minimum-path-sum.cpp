a8bef88e21ea9b5a194784336c013e31b92c9ab3
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-18 11:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (13.91%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -19,10 +19,47 @@ public:
         dp[i][j] = min(up,left);
         return dp[i][j];
     }
+    int tabu(int n, int m, vector<vector<int>>& dp, vector<vector<int>>& grid)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j] = grid[i][j];
+                }
+                else
+                {
+                    int up = grid[i][j];
+                    if(i > 0)
+                    {
+                        up = up + dp[i-1][j];
+                    }
+                    else
+                    {
+                        up = up + 1e9;
+                    }
+                    int left = grid[i][j];
+                    if(j > 0)
+                    {
+                        left = left + dp[i][j-1];
+                    }
+                    else
+                    {
+                        left = left + 1e9;
+                    } 
+                    dp[i][j] = min(left,up);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return memo(n-1,m-1,dp,grid);
+        // return memo(n-1,m-1,dp,grid);
+        return tabu(n,m,dp,grid);
     }
 };

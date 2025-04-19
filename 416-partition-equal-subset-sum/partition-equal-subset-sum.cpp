53f7416efe7c6caa731a52a2cfead6d2fb04f043
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-19 16:26:35 +0530

    Time: 152 ms (26.88%) | Memory: 20.8 MB (46.00%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -23,6 +23,31 @@ public:
         dp[n][target]=pick||nonpick;
         return dp[n][target];
     }
+    bool tabu(int n, int target, vector<vector<bool>>&dp, vector<int>&nums)
+    {
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target)
+        {
+            dp[0][nums[0]]=true;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                bool nonpick=dp[i-1][j];
+                bool pick=false;
+                if(nums[i]<=j)
+                {
+                    pick=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=pick||nonpick;
+            }
+        }
+        return dp[n-1][target];
+    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
@@ -35,7 +60,9 @@ public:
             return false;
         }
         int s=sum/2;
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return memo(n-1,s,dp,nums);
+        // vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        // return memo(n-1,s,dp,nums);
+        vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
+        return tabu(n,s,dp,nums);
     }
 };

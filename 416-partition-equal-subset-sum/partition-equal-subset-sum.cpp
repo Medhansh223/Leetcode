670a44c4d62f120f9c0be2de3f41a9bf19c9a154
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-19 16:16:14 +0530

    Time: 167 ms (22.32%) | Memory: 204 MB (6.90%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    int memo(int n, vector<vector<int>>& dp, vector<int>& nums, int target)
+    bool memo(int n,int target, vector<vector<int>>&dp, vector<int>&nums)
     {
-        if(target == 0)
+        if(target==0)
         {
             return true;
         }
-        if(n == 0)
+        if(n==0)
         {
-            return nums[0] == target;
+            return nums[0]==target;
         }
-        if(dp[n][target] != -1)
+        if(dp[n][target]!=-1)
         {
             return dp[n][target];
         }
-        bool non_pick = memo(n-1,dp,nums,target);
-        bool pick = false;
-        if(nums[n] <= target)
+        bool nonpick=memo(n-1,target,dp,nums);
+        bool pick=false;
+        if(nums[n]<=target)
         {
-            pick = memo(n-1,dp,nums,target - nums[n]);
+            pick=memo(n-1,target-nums[n],dp,nums);
         }
-        dp[n][target] = pick || non_pick;
+        dp[n][target]=pick||nonpick;
         return dp[n][target];
     }
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0;
+        int n=nums.size();
+        int sum=0;
         for(int i=0;i<n;i++)
         {
-            sum = sum + nums[i];
+            sum=sum+nums[i];
         }
-        if(sum % 2 == 1)
+        if(sum%2==1)
         {
             return false;
         }
-        int target = (sum/2);
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return memo(n-1,dp,nums,target);
+        int s=sum/2;
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return memo(n-1,s,dp,nums);
     }
 };

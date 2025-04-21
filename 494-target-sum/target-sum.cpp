1ebd23f9a47b88b29d7f14464383f47ec8a755d0
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-21 10:50:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (50.39%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -27,6 +27,35 @@ public:
         dp[i][sum] = nonpick + pick;
         return dp[i][sum];
     }
+    int tabu(int n, vector<vector<int>>& dp, vector<int>& nums, int sum)
+    {
+        if(nums[0] == 0)
+        {
+            dp[0][0] = 2;
+        }
+        else
+        {
+            dp[0][0] = 1;
+        }
+        if(nums[0] != 0 && nums[0] <= sum)
+        {
+            dp[0][nums[0]] = 1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                int nonpick = dp[i-1][j];
+                int pick = 0;
+                if(nums[i] <= j)
+                {
+                    pick = dp[i-1][j - nums[i]];
+                }
+                dp[i][j] = nonpick + pick;
+            }
+        }
+        return dp[n-1][sum];
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int total = 0;
@@ -39,7 +68,10 @@ public:
         {
             return 0;
         }
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return memo(n-1,dp,nums,sum);
+        // vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        // return memo(n-1,dp,nums,sum);
+
+        vector<vector<int>>dp(n,vector<int>(sum+1,0));
+        return tabu(n,dp,nums,sum);
     }
 };

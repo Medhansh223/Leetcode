7d52197e2ad26934d89e9ead5b938aeff642b3fb
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-22 15:13:55 +0530

    Time: 86 ms (43.79%) | Memory: 84 MB (7.99%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -47,6 +47,33 @@ public:
         }
         return dp[n][m];
     }
+    int space(int n, int m, vector<int>& prev, string s, string t)
+    {
+        for(int i=0;i<=m;i++)
+        {
+            prev[i] = 0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            vector<int>temp(m+1,-1);
+            temp[0] = 0;
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1] == t[j-1])
+                {
+                    temp[j] = 1 + prev[j-1];
+                }
+                else
+                {
+                    int left = temp[j-1];
+                    int up = prev[j];
+                    temp[j] = max(left,up);
+                }
+            }
+            prev = temp;
+        }
+        return prev[m];
+    }
     int longestPalindromeSubseq(string s) {
         int n = s.size();
         string t = "";
@@ -58,10 +85,10 @@ public:
         // vector<vector<int>>dp(n,vector<int>(m,-1));
         // return memo(n-1,m-1,dp,s,t);
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return tabu(n,m,dp,s,t);
-
-        // vector<int>dp(m+1,-1));
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         // return tabu(n,m,dp,s,t);
+
+        vector<int>dp(m+1,-1);
+        return space(n,m,dp,s,t);
     }
 };

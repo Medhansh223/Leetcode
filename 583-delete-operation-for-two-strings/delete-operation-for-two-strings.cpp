3bebc2050c0921c9023f38b62bcbe52f943a8f48
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-23 11:10:59 +0530

    Time: 12 ms (49.56%) | Memory: 16.9 MB (27.35%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -18,12 +18,59 @@ public:
         int left=memo(i,j-1,dp,s1,s2);
         return dp[i][j]=max(up,left);
     }
-    
+    int tabu(int n,int m,vector<vector<int>>&dp,string s1,string s2)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    int up=dp[i-1][j];
+                    int left=dp[i][j-1];
+                    dp[i][j]=max(up,left);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int space(int n,int m,vector<int>&prev,string s1,string s2)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            vector<int>temp(n+1,0);
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    temp[j]=1+prev[j-1];
+                }
+                else
+                {
+                    int up=prev[j];
+                    int left=temp[j-1];
+                    temp[j]=max(up,left);
+                }
+            }
+            prev = temp;
+        }
+        return prev[m];
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        int ans = memo(n-1,m-1,dp,word1,word2);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // int ans = memo(n-1,m-1,dp,word1,word2);
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        int ans = tabu(n,m,dp,word1,word2);
+
+        // vector<int>dp(m+1,0);
+        // int ans = space(n,m,dp,word1,word2);
         return (n-ans) + (m-ans);
     }
 };

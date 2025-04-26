62e063d969430c6fea3c98662cb4f5fdf183f6b7
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-26 11:04:40 +0530

    Time: 7 ms (16.62%) | Memory: 19.8 MB (17.38%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,9 +22,32 @@ public:
         }
         return dp[i][buy]=profit;
     }
+    int tabu(int n,vector<vector<int>>&dp,vector<int>&prices)
+    {
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<2;j++)
+            {
+                int profit=INT_MIN;
+                if(j==1)
+                {
+                    profit=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                else
+                {
+                    profit=max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+                dp[i][j]=profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return memo(0,1,dp,prices);
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return memo(0,1,dp,prices);
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        return tabu(n,dp,prices);
     }
 };

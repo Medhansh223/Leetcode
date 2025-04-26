0d7e8018d97fc9aa42934b443a2094f31ebffdcd
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-26 13:03:30 +0530

    Time: 12 ms (38.68%) | Memory: 19.1 MB (16.37%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -59,25 +59,15 @@ public:
         }
         return dp[0][1][cap-1];
     }
-    int space(int n,vector<vector<int>>&dp,vector<int>&prices) //n*(2*3)
+    //cap=3
+    int space(int n,int cap,vector<vector<int>>&dp,vector<int>&prices) //n*(2*3)
     {
-        for(int i=0;i<2;i++)
-        {
-            dp[i][0]=0;
-        }
-        for(int i=0;i<2;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                dp[i][j]=0;
-            }
-        }
         for(int i=n-1;i>=0;i--)
         {
-            vector<vector<int>>temp(2,vector<int>(3,0));
+            vector<vector<int>>temp(2,vector<int>(cap,0));
             for(int j=0;j<2;j++)
             {
-                for(int k=1;k<3;k++)
+                for(int k=1;k<cap;k++)//k<3
                 {
                     int profit=INT_MIN;
                     if(j==1)
@@ -92,15 +82,15 @@ public:
             }
             dp=temp;
         }
-        return dp[1][2];
+        return dp[1][cap-1];
     }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
         // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
         // return memo(0,1,k,dp,prices);
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));//k=2
-        return tabu(n,k+1,dp,prices);
-        // vector<vector<int>>dp(2,vector<int>(3,-1));
-        // return space(n,dp,prices);
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));//k=2
+        // return tabu(n,k+1,dp,prices);
+        vector<vector<int>>dp(2,vector<int>(k+1,0));//k=2 dp->3
+        return space(n,k+1,dp,prices);//k=3
     }
 };

eb8cf98c0603b480a71bd415c86e0551ef0af003
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-26 10:53:35 +0530

    Time: 5 ms (21.28%) | Memory: 22.1 MB (10.73%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int memo(int i,int day, vector<vector<int>>&dp,vector<int>&prices)
+    int memo(int i,int buy, vector<vector<int>>&dp,vector<int>&prices)
     {
         int n=prices.size();
         if(i==n)
         {
             return 0;
         }
-        if(dp[i][day]!=-1)
+        if(dp[i][buy]!=-1)
         {
-            return dp[i][day];
+            return dp[i][buy];
         }
-        int profit=0;
-        if(day==1)
+        int profit=INT_MIN;
+        if(buy==1)
         {
-            profit = max(-prices[i]+memo(i+1,0,dp,prices),memo(i+1,1,dp,prices));
+            profit=max(-prices[i]+memo(i+1,0,dp,prices),memo(i+1,1,dp,prices));
         }
         else
         {
             profit=max(prices[i]+memo(i+1,1,dp,prices),memo(i+1,0,dp,prices));
         }
-        dp[i][day]=profit;
-        return dp[i][day];
+        return dp[i][buy]=profit;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
@@ -29,5 +28,3 @@ public:
         return memo(0,1,dp,prices);
     }
 };
-//1-->buy
-//0-->sell (not buy)

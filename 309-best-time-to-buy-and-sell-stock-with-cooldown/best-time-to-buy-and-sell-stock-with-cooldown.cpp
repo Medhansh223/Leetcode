d4e3f4cfa63b18513505c7a0cbcbdf501532fd38
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-27 17:33:38 +0530

    Time: 4 ms (21.13%) | Memory: 16.2 MB (35.42%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -23,9 +23,35 @@ public:
         dp[i][buy] = profit;
         return dp[i][buy];
     }
+    int tabu(int n, vector<vector<int>>& dp, vector<int>& prices)
+    {
+        dp[n][0] = dp[n][1] = 0;
+        dp[n+1][0] = dp[n+1][1] = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                int profit = 0;
+                if(j == 1)
+                {
+                    profit = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+                }
+                else
+                {
+                    profit = max(prices[i] + dp[i+2][1], dp[i+1][0]);
+                }
+                dp[i][j] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return memo(0,1,dp,prices);
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return memo(0,1,dp,prices);
+
+        vector<vector<int>>dp(n+2,vector<int>(2,-1));
+        return tabu(n,dp,prices);
     }
 };

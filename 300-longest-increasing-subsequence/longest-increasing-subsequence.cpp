a4c625bb20697de32dd79652aec01103f084e579
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-28 11:23:27 +0530

    Time: 279 ms (30.01%) | Memory: 296.5 MB (11.23%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -20,9 +20,29 @@ public:
         dp[i][prev+1] = max(not_take,take);
         return dp[i][prev+1];
     }
+    int tabu(int n, vector<int>& nums, vector<vector<int>>& dp)
+    {
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i-1;j>=-1;j--)
+            {
+                int not_take = dp[i+1][j+1];
+                int take = INT_MIN;
+                if(j == -1 || nums[j] < nums[i])
+                {
+                    take = 1 + dp[i+1][i+1];
+                }
+                dp[i][j+1] = max(not_take,take);
+            }
+        }
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return memo(0,-1,nums,dp);
+        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        // return memo(0,-1,nums,dp);
+
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        return tabu(n,nums,dp);
     }
 };

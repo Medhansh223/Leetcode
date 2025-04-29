a6feb557bbbf6aeb9bb8acb0823e10438b29c59d
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-29 11:39:04 +0530

    Time: 290 ms (27.16%) | Memory: 296.5 MB (11.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int memo(int i ,int prev, vector<int>& nums, vector<vector<int>>& dp)
+    int memo(int i, int prev, vector<vector<int>>& dp, vector<int>& nums)
     {
-        int n = nums.size();
-        if(i == n)
+        if(i == nums.size())
         {
             return 0;
         }
@@ -11,60 +10,38 @@ public:
         {
             return dp[i][prev+1];
         }
-        int not_take = 0 + memo(i+1,prev,nums,dp);
+        int nottake = memo(i+1,prev,dp,nums);
         int take = INT_MIN;
         if(prev == -1 || nums[i] > nums[prev])
         {
-            take = 1 + memo(i+1,i,nums,dp);
+            take = 1 + memo(i+1,i,dp,nums);
         }
-        dp[i][prev+1] = max(not_take,take);
+        dp[i][prev+1] = max(take,nottake);
         return dp[i][prev+1];
-    }
-    int tabu(int n, vector<int>& nums, vector<vector<int>>& dp)
+    } 
+    int tabu(int n, vector<vector<int>>& dp, vector<int>& nums)
     {
         for(int i=n-1;i>=0;i--)
         {
-            for(int j=i-1;j>=-1;j--)
+            for(int j=i;j>=-1;j--)
             {
-                int not_take = dp[i+1][j+1];
+                int nottake = dp[i+1][j+1];
                 int take = INT_MIN;
-                if(j == -1 || nums[j] < nums[i])
+                if(j == -1 || nums[i] > nums[j])
                 {
                     take = 1 + dp[i+1][i+1];
                 }
-                dp[i][j+1] = max(not_take,take);
+                dp[i][j+1] = max(take,nottake);
             }
         }
         return dp[0][0];
     }
-    int space(int n, vector<int>& nums, vector<int>& dp)
-    {
-        for(int i=n-1;i>=0;i--)
-        {
-            vector<int>temp(n+1,0);
-            for(int j=i-1;j>=-1;j--)
-            {
-                int not_take = dp[j+1];
-                int take = INT_MIN;
-                if(j == -1 || nums[j] < nums[i])
-                {
-                    take = 1 + dp[i+1];
-                }
-                temp[j+1] = max(not_take,take);
-            }
-            dp = temp;
-        }
-        return dp[0];
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        // return memo(0,-1,nums,dp);
+        // return memo(0,-1,dp,nums);
 
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        // return tabu(n,nums,dp);
-
-        vector<int>dp(n+1,0);
-        return space(n,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        return tabu(n,dp,nums);
     }
 };

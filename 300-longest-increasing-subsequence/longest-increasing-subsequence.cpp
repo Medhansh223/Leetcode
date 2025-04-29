838e67a5015b884cc8e70e0644742e382ed0b25a
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-04-29 13:16:17 +0530

    Time: 4 ms (78.32%) | Memory: 14.2 MB (78.95%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,69 +1,22 @@
 class Solution {
 public:
-    int memo(int i, int prev, vector<vector<int>>& dp, vector<int>& nums)
-    {
-        if(i == nums.size())
-        {
-            return 0;
-        }
-        if(dp[i][prev+1] != -1)
-        {
-            return dp[i][prev+1];
-        }
-        int nottake = memo(i+1,prev,dp,nums);
-        int take = INT_MIN;
-        if(prev == -1 || nums[i] > nums[prev])
-        {
-            take = 1 + memo(i+1,i,dp,nums);
-        }
-        dp[i][prev+1] = max(take,nottake);
-        return dp[i][prev+1];
-    } 
-    int tabu(int n, vector<vector<int>>& dp, vector<int>& nums)
-    {
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i;j>=-1;j--)
-            {
-                int nottake = dp[i+1][j+1];
-                int take = INT_MIN;
-                if(j == -1 || nums[i] > nums[j])
-                {
-                    take = 1 + dp[i+1][i+1];
-                }
-                dp[i][j+1] = max(take,nottake);
-            }
-        }
-        return dp[0][0];
-    }
-    int space(int n, vector<int>& dp, vector<int>& nums)
-    {
-        for(int i=n-1;i>=0;i--)
-        {
-            vector<int>temp(n+1,0);
-            for(int j=i;j>=-1;j--)
-            {
-                int nottake = dp[j+1];
-                int take = INT_MIN;
-                if(j == -1 || nums[i] > nums[j])
-                {
-                    take = 1 + dp[i+1];
-                }
-                temp[j+1] = max(take,nottake);
-            }
-            dp = temp;
-        }
-        return dp[0];
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        // return memo(0,-1,dp,nums);
-
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        // return tabu(n,dp,nums);
-
-        vector<int>dp(n+1,0);
-        return space(n,dp,nums);
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len = 1;
+        for(int i=1;i<nums.size();i++) 
+        {
+            if(nums[i]>temp.back()) 
+            {
+                temp.push_back(nums[i]);
+                len++;
+            } 
+            else 
+            {
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+        return len;
     }
 };

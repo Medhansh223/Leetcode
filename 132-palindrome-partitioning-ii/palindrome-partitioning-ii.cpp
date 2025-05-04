91529a6c105c85bf3ff1b30f7afdb4744b5cd24e
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-04 13:30:45 +0530

    Time: 1515 ms (38.50%) | Memory: 9.2 MB (86.71%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -13,31 +13,51 @@ public:
         }
         return true;
     }
-    int memo(int i, int j, string& s, vector<int>& dp)
+    // int memo(int i, int j, string& s, vector<int>& dp)
+    // {
+    //     if(i >= j || isPalindrome(i,j,s)) 
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i] != -1)
+    //     {
+    //         return dp[i];
+    //     }
+    //     int mini = INT_MAX;
+    //     for(int k=i;k<j;k++)
+    //     {
+    //         if(isPalindrome(i,k,s) == true)
+    //         {
+    //             int cost = 1 + memo(k+1,j,s,dp);
+    //             mini = min(mini,cost);
+    //         }
+    //     }
+    //     dp[i] = mini;
+    //     return dp[i];
+    // }
+    int tabu(int n, string& s, vector<int>& dp)
     {
-        if(i >= j || isPalindrome(i,j,s)) 
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--)
         {
-            return 0;
-        }
-        if(dp[i] != -1)
-        {
-            return dp[i];
-        }
-        int mini = INT_MAX;
-        for(int k=i;k<j;k++)
-        {
-            if(isPalindrome(i,k,s) == true)
+            int mini = INT_MAX;
+            for(int j=i;j<n;j++)
             {
-                int cost = 1 + memo(k+1,j,s,dp);
-                mini = min(mini,cost);
+                if(isPalindrome(i,j,s) == true)
+                {
+                    int cost = 1 + dp[j+1];
+                    mini = min(mini,cost);
+                }
             }
+            dp[i] = mini;
         }
-        dp[i] = mini;
-        return dp[i];
+        return dp[0];
     }
     int minCut(string s) {
         int n = s.length();
-        vector<int>dp(n+1,-1);   
-        return memo(0,n-1,s,dp);
+        // vector<int>dp(n+1,-1);   
+        // return memo(0,n-1,s,dp);
+        vector<int>dp(n+1,-1);
+        return tabu(n,s,dp) - 1;
     }
 };

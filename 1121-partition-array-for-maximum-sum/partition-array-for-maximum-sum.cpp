f05e554ff7d0f1f7d83c66383cdf86ca79feb9bc
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-05 10:50:51 +0530

    Time: 3 ms (85.89%) | Memory: 12.5 MB (29.37%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -23,9 +23,30 @@ public:
         dp[ind]=maxsum;
         return dp[ind];
     }
+    int tabu(int n,vector<int>&dp,vector<int>&arr,int k)
+    {
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            int maxi=INT_MIN;
+            int maxsum=INT_MIN;
+            int len=0;
+            for(int j=i;j<min(n,i+k);j++)
+            {
+                len++;
+                maxi=max(maxi,arr[j]);
+                int sum=len*maxi+dp[j+1];
+                maxsum=max(maxsum,sum);
+            }
+            dp[i]=maxsum;
+        }
+        return dp[0];
+    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int>dp(n,-1);
-        return memo(0,n,dp,arr,k);
+        // vector<int>dp(n,-1);
+        // return memo(0,n,dp,arr,k);
+        vector<int>dp(n+1,-1);
+        return tabu(n,dp,arr,k);
     }
 };

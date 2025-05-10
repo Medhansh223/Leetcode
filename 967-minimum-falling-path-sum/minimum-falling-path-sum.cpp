305af1d8ca1663ab08e274734a7ff93895d09b79
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-10 22:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (18.01%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    int tabu(int n, int m, vector<vector<int>>& dp, vector<vector<int>>& matrix)
+    int memo(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& matrix)
     {
-        for(int i=0;i<m;i++)
+        int n = matrix.size();
+        if(j < 0 || j >= n)
+        {
+            return 1e9;
+        }
+        if(i == 0)
+        {
+            return matrix[i][j];
+        }
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        int up = matrix[i][j] + memo(i-1,j,dp,matrix);
+        int ld = matrix[i][j] + memo(i-1,j-1,dp,matrix);
+        int rd = matrix[i][j] + memo(i-1,j+1,dp,matrix);
+        dp[i][j] = min({up,ld,rd});
+        return dp[i][j];
+    }
+    int tabu(int n, vector<vector<int>>& dp, vector<vector<int>>& matrix)
+    {
+        for(int i=0;i<n;i++)
         {
             dp[0][i] = matrix[0][i];
         }
         for(int i=1;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 int ld = matrix[i][j];
                 if(j-1 >= 0)
@@ -21,7 +42,7 @@ public:
                 }
                 int up = matrix[i][j] + dp[i-1][j];
                 int rd = matrix[i][j];
-                if(j+1 < m)
+                if(j+1 < n)
                 {
                     rd = rd + dp[i-1][j+1];
                 }
@@ -34,48 +55,15 @@ public:
         }
         return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
-    int space(int n, int m, vector<int>& prev, vector<vector<int>>& matrix)
-    {   
-        for(int i=0;i<m;i++)
-        {
-            prev[i] = matrix[0][i];
-        }
-        for(int i=1;i<n;i++)
-        {
-            vector<int>temp(m,-1);
-            for(int j=0;j<m;j++)
-            {
-                int ld = matrix[i][j];
-                if(j-1 >= 0)
-                {
-                    ld = ld + prev[j-1];
-                }
-                else
-                {
-                    ld = ld + 1e9;
-                }
-                int up = matrix[i][j] + prev[j];
-                int rd = matrix[i][j];
-                if(j+1 < m)
-                {
-                    rd = rd + prev[j+1];
-                }
-                else
-                {
-                    rd = rd + 1e9;
-                }
-                temp[j] = min(ld,min(up,rd));
-            }
-            prev = temp;
-        }
-        return *min_element(prev.begin(),prev.end());
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int m = matrix[0].size();
-        // vector<vector<int>>dp(n,vector<int>(m,-1));
-        // return tabu(n,m,dp,matrix);
-        vector<int>dp(m,-1);
-        return space(n,m,dp,matrix);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        // int ans = 1e9;
+        // for(int i=0;i<n;i++)
+        // {
+        //     ans = min(ans,memo(n-1,i,dp,matrix));
+        // }
+        // return ans;
+        return tabu(n,dp,matrix);
     }
 };

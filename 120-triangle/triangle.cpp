17ba4755d6d13de493aa271f1a24aba533815de4
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-10 21:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (49.23%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,75 +1,42 @@
 class Solution {
 public:
-    int memo(int i, int j, vector<vector<int>>&dp, vector<vector<int>>&triangle)
+    int memo(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& triangle)
     {
-        int n=triangle.size();
-        if(i==n-1)
+        int n = triangle.size();
+        if(i == n-1)
         {
             return triangle[n-1][j];
         }
-        if(dp[i][j]!=-1)
+        if(dp[i][j] != -1)
         {
             return dp[i][j];
         }
-        int down=triangle[i][j] + memo(i+1,j,dp,triangle);
-        int dj=triangle[i][j] + memo(i+1,j+1,dp,triangle);
-        dp[i][j]=min(down,dj);
+        int down = triangle[i][j] + memo(i+1,j,dp,triangle);
+        int dj = triangle[i][j] + memo(i+1,j+1,dp,triangle);
+        dp[i][j] = min(down,dj);
         return dp[i][j];
     }
-    int tabu(vector<vector<int>>&dp,vector<vector<int>>&triangle)
+    int tabu(int n, vector<vector<int>>& dp, vector<vector<int>>& triangle)
     {
-        int n=triangle.size();
-        dp[0][0]=triangle[0][0];
-        int mini=INT_MAX;
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<=i;j++)
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
             {
-                int up = INT_MAX, ld = INT_MAX;
-
-                if (j < i) up = triangle[i][j] + dp[i - 1][j]; // from directly above
-                if (j > 0) ld = triangle[i][j] + dp[i - 1][j - 1]; // from left diagonal);
-                dp[i][j]=min(up,ld);
+                int down = triangle[i][j] + dp[i+1][j];
+                int dj = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,dj);
             }
         }
-        // return *min_element(dp[n - 1].begin(), dp[n - 1].begin() + n);
-        int s=dp[n-1].size();
-        for(int i=0;i<s;i++)
-        {   
-            mini=min(mini,dp[n-1][i]);
-        }
-        return mini;
-    }
-    int space(vector<int>& prev, vector<vector<int>>& triangle)
-    {
-        prev[0]=triangle[0][0];
-        for(int i=1;i<triangle.size();i++)
-        {
-            vector<int>temp(triangle.size(),-1);
-            for(int j=0;j<=i;j++)
-            {
-                int up = INT_MAX;
-                int ld = INT_MAX;
-                if(j<i)
-                {
-                    up = triangle[i][j] + prev[j];
-                }
-                if(j>0)
-                {
-                    ld = triangle[i][j] + prev[j-1];
-                }
-                temp[j] = min(up,ld);
-            }
-            prev = temp;
-        }
-        return *min_element(prev.begin(),prev.end());
+        return dp[0][0];
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
         // return memo(0,0,dp,triangle);
-        // return tabu(dp,triangle);
-        vector<int>dp(n,-1);
-        return space(dp,triangle);
+        return tabu(n,dp,triangle);
     }
 };

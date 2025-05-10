4783e7814b0d6872a94f9279a02bde4fe68c0933
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-10 22:12:53 +0530

    Time: 3 ms (33.10%) | Memory: 15 MB (38.93%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -55,15 +55,53 @@ public:
         }
         return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
+    int space(int n, vector<int>& prev, vector<vector<int>>& matrix)
+    {   
+        for(int i=0;i<n;i++)
+        {
+            prev[i] = matrix[0][i];
+        }
+        for(int i=1;i<n;i++)
+        {
+            vector<int>temp(n,-1);
+            for(int j=0;j<n;j++)
+            {
+                int ld = matrix[i][j];
+                if(j-1 >= 0)
+                {
+                    ld = ld + prev[j-1];
+                }
+                else
+                {
+                    ld = ld + 1e9;
+                }
+                int up = matrix[i][j] + prev[j];
+                int rd = matrix[i][j];
+                if(j+1 < n)
+                {
+                    rd = rd + prev[j+1];
+                }
+                else
+                {
+                    rd = rd + 1e9;
+                }
+                temp[j] = min(ld,min(up,rd));
+            }
+            prev = temp;
+        }
+        return *min_element(prev.begin(),prev.end());
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
         // int ans = 1e9;
         // for(int i=0;i<n;i++)
         // {
         //     ans = min(ans,memo(n-1,i,dp,matrix));
         // }
         // return ans;
-        return tabu(n,dp,matrix);
+        // return tabu(n,dp,matrix);
+        vector<int>dp(n,-1);
+        return space(n,dp,matrix);
     }
 };

f3816305ce5f8a237f4a88193affc01f10822b45
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-10 15:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (51.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
-    int memoization(int n, vector<int>& dp)
+    int memo(int i, vector<int>& dp)
     {
-        if(n==0 || n==1)
+        if(i == 0 || i == 1)
         {
             return 1;
         }
-        if(dp[n]!=-1)
+        if(dp[i] != -1)
         {
-            return dp[n];
+            return dp[i];
         }
-        dp[n]=memoization(n-1,dp)+memoization(n-2,dp);
-        return dp[n];
-    }
+        int one_step = memo(i-1,dp);
+        int two_step = memo(i-2,dp);
+        dp[i] = one_step + two_step;
+        return dp[i];
 
-    int tabulation(int n, vector<int>& dp)
-    {
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-
-    int optimization(int n)
-    {
-        if(n==0 || n==1)
-        {
-            return 1;
-        }
-        int prev1=1;
-        int prev2=1;
-        int curr;
-        for(int i=2;i<=n;i++)
-        {
-            curr=prev1+prev2;
-            prev1=prev2;
-            prev2=curr;
-        }
-        return prev2;
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        // int ans1=memoization(n,dp);
-        // int ans2=tabulation(n,dp);
-        int ans3=optimization(n);
-        return ans3;
+        return memo(n,dp);
     }
 };

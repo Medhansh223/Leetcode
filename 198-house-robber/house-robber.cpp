86f5e349f5f90d6df79127a5185dbe6938db1c50
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-10 16:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (45.37%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,63 +1,27 @@
 class Solution {
 public:
-    int memo(vector<int>&dp,int n,vector<int>&nums)
+    int memo(int i, vector<int>& dp, vector<int>& nums)
     {
-        if(n==0)
+        if(i == 0)
         {
-            return nums[n];
+            return nums[0];
         }
-        if(dp[n]!=-1)
+        if(dp[i] != - 1)
         {
-            return dp[n];
+            return dp[i];
         }
-        int pick=nums[n];
-        if(n-2>=0)
+        int not_pick = memo(i-1,dp,nums);
+        int pick = nums[i];
+        if(i >= 2)
         {
-            pick=pick+memo(dp,n-2,nums);
+            pick = pick + memo(i-2,dp,nums);
         }
-        int nonpick=memo(dp,n-1,nums);
-        dp[n]=max(pick,nonpick);
-        return dp[n];
-    }
-    int tabu(vector<int>&dp,int n,vector<int>&nums)
-    {
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++)
-        {
-            int pick=nums[i];
-            if(i-2>=0)
-            {
-                pick=pick+dp[i-2];
-            }
-            int nonpick=dp[i-1];
-            dp[i]=max(pick,nonpick);
-        }
-        return dp[n-1];
-    }
-    int space(int n,vector<int>&nums)
-    {
-        int prev1=nums[0];
-        int prev2=0;
-        int curr;
-        for(int i=1;i<n;i++)
-        {
-            int pick=nums[i];
-            if(i-2>=0)
-            {
-                pick=pick+prev2;
-            }
-            int nonpick=prev1;
-            curr=max(pick,nonpick);
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        dp[i] = max(pick,not_pick);
+        return dp[i];
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         vector<int>dp(n,-1);
-        // return memo(dp,n-1,nums);
-        // return tabu(dp,n,nums);
-        return space(n,nums);
+        return memo(n-1,dp,nums);
     }
 };

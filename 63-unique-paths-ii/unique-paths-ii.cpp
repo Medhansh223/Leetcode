405f55bbef9f3bda859c743c062c340b2be9b470
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-10 17:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (70.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int memoization(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& obstacleGrid)
+    int memo(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& obstacleGrid)
     {
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+        if(i >=0 && j>=0 && obstacleGrid[i][j] == 1)
         {
             return 0;
         }
-        if(i==0 && j==0)
+        if(i == 0 && j == 0)
         {
             return 1;
         }
-        if(i<0 || j<0)
+        if(i < 0 || j < 0)
         {
             return 0;
         }
-        if(dp[i][j]!=-1)
+        if(dp[i][j] != -1)
         {
             return dp[i][j];
         }
-        int up = memoization(i-1,j,dp,obstacleGrid);
-        int left = memoization(i,j-1,dp,obstacleGrid);
-        dp[i][j] = up+left;
+        int left = memo(i,j-1,dp,obstacleGrid);
+        int up = memo(i-1,j,dp,obstacleGrid);
+        dp[i][j] = left + up;
         return dp[i][j];
     }
-
-    int tabulation(int m, int n, vector<vector<int>>& dp, vector<vector<int>>& obstacleGrid)
+    int tabu(int m, int n, vector<vector<int>>& dp, vector<vector<int>>& obstacleGrid)
     {
         for(int i=0;i<m;i++)
         {
@@ -34,30 +33,29 @@ public:
                 {
                     dp[i][j]=0;
                 }
-                else if(i==0 && j==0)
+                else if(i == 0 && j == 0)
                 {
-                    dp[i][j]=1;
+                    dp[i][j] = 1;
                 }
                 else
                 {
-                    int up=0;
-                    int left=0;
-                    if(i>0)
+                    int left = 0;
+                    if(j-1 >= 0)
                     {
-                        up=dp[i-1][j];
+                        left = dp[i][j-1];
                     }
-                    if(j>0)
+                    int up = 0;
+                    if(i-1 >= 0)
                     {
-                        left=dp[i][j-1];
+                        up = dp[i-1][j];
                     }
-                    dp[i][j]=up+left;
+                    dp[i][j] = left + up;
                 }
             }
         }
         return dp[m-1][n-1];
     }
-
-    int optimization(int m, int n, vector<int>& prev, vector<vector<int>>& obstacleGrid)
+    int space(int m, int n, vector<int>& dp, vector<vector<int>>& obstacleGrid)
     {
         for(int i=0;i<m;i++)
         {
@@ -68,38 +66,36 @@ public:
                 {
                     temp[j]=0;
                 }
-                else if(i==0 && j==0)
+                else if(i == 0 && j == 0)
                 {
-                    temp[j]=1;
+                    temp[j] = 1;
                 }
                 else
                 {
-                    int up=0;
-                    int left=0;
-                    if(i>0)
+                    int left = 0;
+                    if(j-1 >= 0)
                     {
-                        up=prev[j];
+                        left = temp[j-1];
                     }
-                    if(j>0)
+                    int up = 0;
+                    if(i-1 >= 0)
                     {
-                        left=temp[j-1];
+                        up = dp[j];
                     }
-                    temp[j]=up+left;
+                    temp[j] = left + up;
                 }
             }
-            prev=temp;
+            dp = temp;
         }
-        return prev[n-1];
+        return dp[n-1];
     }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
         // vector<vector<int>>dp(m,vector<int>(n,-1));
-        // int ans1 = memoization(m-1,n-1,dp,obstacleGrid);
-        // int ans2 = tabulation(m,n,dp,obstacleGrid);
+        // return memo(m-1,n-1,dp,obstacleGrid);
+        // return tabu(m,n,dp,obstacleGrid);
         vector<int>dp(n,-1);
-        int ans3 = optimization(m,n,dp,obstacleGrid);
-        return ans3;
+        return space(m,n,dp,obstacleGrid);
     }
 };

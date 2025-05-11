092fa55dbf59cfcd9d43c574afbc718a0b8b5399
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-11 15:38:51 +0530

    Time: 139 ms (32.26%) | Memory: 110.6 MB (15.61%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,68 +1,51 @@
 class Solution {
 public:
-    bool memo(int n,int target, vector<vector<int>>&dp, vector<int>&nums)
+    int memo(int i, int target, vector<vector<int>>& dp, vector<int>& nums)
     {
-        if(target==0)
+        if(i == 0)
         {
-            return true;
-        }
-        if(n==0)
-        {
-            return nums[0]==target;
-        }
-        if(dp[n][target]!=-1)
-        {
-            return dp[n][target];
-        }
-        bool nonpick=memo(n-1,target,dp,nums);
-        bool pick=false;
-        if(nums[n]<=target)
-        {
-            pick=memo(n-1,target-nums[n],dp,nums);
-        }
-        dp[n][target]=pick||nonpick;
-        return dp[n][target];
-    }
-    bool tabu(int n, int target, vector<vector<bool>>&dp, vector<int>&nums)
-    {
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=true;
-        }
-        if(nums[0]<=target)
-        {
-            dp[0][nums[0]]=true;
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<=target;j++)
+            if(nums[0] == 0 && target == 0)
             {
-                bool nonpick=dp[i-1][j];
-                bool pick=false;
-                if(nums[i]<=j)
-                {
-                    pick=dp[i-1][j-nums[i]];
-                }
-                dp[i][j]=pick||nonpick;
+                return true;
             }
+            if(target == 0 || nums[0] == target)
+            {
+                return true;
+            }
+            return false;
         }
-        return dp[n-1][target];
+        if(dp[i][target] != -1)
+        {
+            return dp[i][target];
+        }
+        bool not_pick = memo(i-1,target,dp,nums);
+        bool pick = false;
+        if(nums[i] <= target)
+        {
+            pick = memo(i-1,target - nums[i],dp,nums);
+        }
+        dp[i][target] = pick || not_pick;
+        return dp[i][target];
     }
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
+        int n = nums.size();
+        int sum = 0;
         for(int i=0;i<n;i++)
         {
-            sum=sum+nums[i];
+            sum = sum + nums[i];
         }
-        if(sum%2==1)
+        if(sum % 2 == 1)
         {
             return false;
         }
-        int s=sum/2;
-        // vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        // return memo(n-1,s,dp,nums);
-        vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
-        return tabu(n,s,dp,nums);
+        int target = sum / 2;
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return memo(n-1,target,dp,nums);
+
+        // vector<vector<int>>dp(n,vector<int>(target+1,false));
+        // return memo(n,target,dp,nums);
+
+        // vector<int>dp(target+1,false);
+        // return memo(n,target,dp,nums);
     }
 };

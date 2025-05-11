68afa00a0818694eebcf64e7a1b348b7a3b256f3
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-11 16:22:45 +0530

    Time: 44 ms (38.21%) | Memory: 48.8 MB (38.21%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -26,10 +26,42 @@ public:
         dp[i][amount] = min(pick,not_pick);
         return dp[i][amount];
     }
+    int tabu(int n, int amount, vector<vector<int>>& dp, vector<int>& coins)
+    {
+        for(int i=0;i<=amount;i++)
+        {
+            if(i % coins[0] == 0)
+            {
+                dp[0][i] = i / coins[0];
+            }
+            else
+            {
+                dp[0][i] = 1e9;
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                int not_pick = dp[i-1][j];
+                int pick = INT_MAX;
+                if(coins[i] <= j)
+                {
+                    pick = 1 + dp[i][j - coins[i]];
+                }
+                dp[i][j] = min(pick,not_pick);
+            }
+        }
+        return dp[n-1][amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = memo(n-1,amount,dp,coins);
+        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        // int ans = memo(n-1,amount,dp,coins);
+
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        int ans = tabu(n,amount,dp,coins);
+
         if(ans == 1e9)
         {
             return -1;

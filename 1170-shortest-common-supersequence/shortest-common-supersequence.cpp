6f25e157eaeb1c9de022f8756d5dbdbba0271f4f
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-05-13 16:11:56 +0530

    Time: 27 ms (40.42%) | Memory: 31 MB (7.71%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    int tabu(int n, int m, vector<vector<int>>& dp, string &s1, string &s2)
+    int tabu(int n, int m, string str1, string str2, vector<vector<int>>& dp)
     {
         for(int i=0;i<=m;i++)
         {
             dp[0][i] = 0;
         }
-        for(int i=0;i<=n;i++)
+        for(int j=0;j<=n;j++)
         {
-            dp[i][0] = 0;
+            dp[j][0] = 0;
         }
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(s1[i-1] == s2[j-1])
+                if(str1[i-1] == str2[j-1])
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else
                 {
                     int left = dp[i][j-1];
-                    int up = dp[i-1][j];
-                    dp[i][j] = max(left,up);
+                    int top = dp[i-1][j];
+                    dp[i][j] = max(left,top);
                 }
             }
         }
         return dp[n][m];
     }
-    
     string shortestCommonSupersequence(string str1, string str2) {
         int n = str1.size();
         int m = str2.size();
-        vector<vector<int>>dp(n+1,(vector<int>(m+1,0)));
-        int length = n + m - tabu(n,m,dp,str1,str2);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int length = n + m - tabu(n,m,str1,str2,dp);
         string s = "";
         for(int i=0;i<length;i++)
         {
             s = s + '$';
         }
-        int index = length - 1;
         int i = n;
         int j = m;
+        int index = length - 1;
         while(i > 0 && j > 0)
         {
             if(str1[i-1] == str2[j-1])
@@ -64,13 +63,13 @@ public:
                 j--;
             }
         }
-        while(i>0)
+        while(i > 0)
         {
             s[index] = str1[i-1];
             index--;
             i--;
         }
-        while(j>0)
+        while(j > 0)
         {
             s[index] = str2[j-1];
             index--;

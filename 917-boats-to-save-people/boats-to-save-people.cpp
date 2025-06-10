a19c35e1195394186bde8548c2c0902052e485dd
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-10 16:04:10 +0530

    Time: 15 ms (73.43%) | Memory: 45.9 MB (36.37%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int n=people.size();
+        int n = people.size();
         sort(people.begin(),people.end());
-        int boats=0;
-        int left = 0;
-        int right = n-1;
-        while(left <= right)
+        int boats = 0;
+        int l = 0;
+        int r = n-1;
+        while(l <= r)
         {
-            int sum = people[left] + people[right];
-            if(sum <= limit)
+            if(people[l] + people[r] <= limit)
             {
                 boats++;
-                left++;
-                right--;
+                l++;
+                r--;
             }
             else
             {
                 boats++;
-                right--;
+                r--;
             }
         }
         return boats;

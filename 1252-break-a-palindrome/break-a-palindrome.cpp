0bf38c8372bae448300b5d18e2f4e7c016eeefca
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-10 16:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (64.93%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.cpp
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
-        int n=palindrome.size();
+        int n = palindrome.size();
         for(int i=0;i<n/2;i++)
         {
-            if(palindrome[i]!='a')
+            if(palindrome[i] != 'a')
             {
-                palindrome[i]='a';
+                palindrome[i] = 'a';
                 return palindrome;
             }
         }
-        if(n==1)
+        if(n == 1)
         {
-            palindrome="";
+            return "";
         }
         else
         {
-            palindrome[n-1]='b';
+            palindrome[n-1] = 'b';
         }
         return palindrome;
     }

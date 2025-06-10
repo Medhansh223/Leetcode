86770818614e91ac68f40689d97781ed2894caec
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-11 00:12:55 +0530

    Time: 2 ms (87.73%) | Memory: 12.9 MB (90.37%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -3,15 +3,15 @@ public:
     int partitionString(string s) {
         int n = s.length();
         vector<int>num(26,-1);
-        int last = 0;
         int count = 0;
+        int curr = 0;
         for(int i=0;i<n;i++)
         {
             char ch = s[i];
-            if(num[ch - 'a'] >= last)
+            if(num[ch - 'a'] >= curr)
             {
                 count++;
-                last = i;
+                curr = i;
             }
             num[ch - 'a'] = i;
         }

4f373e5eb6f09bf376ef51ca58a1124bcb44c1fa
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-15 16:47:53 +0530

    Time: 4 ms (53.05%) | Memory: 50.5 MB (43.51%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        int sumeven = 0;
+        int n=nums.size();
+        int q=queries.size();
+        int sum=0;
+        vector<int>result;
         for(int i=0;i<n;i++)
         {
-            if(nums[i] % 2 == 0)
+            if(nums[i]%2==0)
             {
-                sumeven = sumeven + nums[i];
+                sum=sum+nums[i];
             }
         }
-        vector<int>result;
-        int q = queries.size();
         for(int i=0;i<q;i++)
         {
-            int val = queries[i][0];
-            int index = queries[i][1];
-            if(nums[index] % 2 == 0)
+            int val=queries[i][0];
+            int ind=queries[i][1];
+            if(nums[ind]%2==0)
             {
-                sumeven = sumeven - nums[index];
+                sum=sum-nums[ind];
             }
-            nums[index] = nums[index] + val;
-            if(nums[index] % 2 == 0)
+            nums[ind]=nums[ind]+val;
+            if(nums[ind]%2==0)
             {
-                sumeven = sumeven + nums[index];
+                sum=sum+nums[ind];
             }
-            result.push_back(sumeven);
+            result.push_back(sum);
         }
         return result;
     }

e572180d55eae7c71f65818f59ac5fea427c416c
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-19 09:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.15%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -18,12 +18,12 @@ public:
         int high=n-2;
         while(low<=high)
         {
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
             {
                 return nums[mid];
-            } 
-            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
+            }
+            else if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1]))
             {
                 low=mid+1;
             }

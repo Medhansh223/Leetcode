c3460c163df52ffcd48beb17de0f05c67f0e949c
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-20 18:41:01 +0530

    Time: 203 ms (8.24%) | Memory: 58.8 MB (5.50%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    void dfs(int row, int col, queue<pair<int,int>>& q, vector<vector<int>>& visited, vector<vector<int>>& grid)
+    void dfs(int r,int c,vector<vector<int>>&vis,vector<vector<int>>&grid)
     {
         int n=grid.size();
         int m=grid[0].size();
-        vector<int>drow={-1,0,+1,0};
-        vector<int>dcol={0,+1,0,-1};
-        visited[row][col]=1;
+        vector<int>row={-1,0,+1,0};
+        vector<int>col={0,+1,0,-1};
+        vis[r][c]=1;
         for(int i=0;i<4;i++)
         {
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]!=1)
+            int nrow=r+row[i];
+            int ncol=c+col[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1)
             {
-                dfs(nrow,ncol,q,visited,grid);
+                dfs(nrow,ncol,vis,grid);
             }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int>(m));
-        queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(i==0 || i==n-1 || j==0 || j==m-1)
+                if(i==0 || j==0 || i==n-1 || j==m-1)
                 {
-                    if(grid[i][j]==1 && visited[i][j]!=1)
+                    if(grid[i][j]==1 && !vis[i][j])
                     {
-                        dfs(i,j,q,visited,grid);
+                        dfs(i,j,vis,grid);
                     }
                 }
             }
@@ -40,7 +39,7 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==1 && visited[i][j]!=1)
+                if(grid[i][j]==1 && !vis[i][j])
                 {
                     count++;
                 }

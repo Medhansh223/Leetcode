b51aa6c52559d965c8ece6add857f5891a8b7caa
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-06-22 10:30:31 +0530

    Time: 21 ms (10.76%) | Memory: 37.3 MB (39.55%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
+        int mod=1e9+7;
         vector<vector<pair<int,int>>>adj(n);
         for(auto it:roads)
         {
             int u=it[0];
             int v=it[1];
-            int wt=it[2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+            int w=it[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
         priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
         pq.push({0,0});
-        vector<long long>distance(n,LLONG_MAX);
-        distance[0]=0;
+        vector<long long>dis(n,LLONG_MAX);
+        dis[0]=0;
         vector<int>path(n,0);
         path[0]=1;
-        int mod=1e9+7;
         while(!pq.empty())
         {
-            long long dis=pq.top().first;
             int node=pq.top().second;
+            long long d=pq.top().first;
             pq.pop();
             for(auto it:adj[node])
             {
-                int adjnode=it.first;
-                int wt=it.second;
-                if(dis+wt<distance[adjnode])
+                int ele=it.first;
+                long long wt=it.second;
+                if(d+wt<dis[ele])
                 {
-                    distance[adjnode]=dis+wt;
-                    pq.push({distance[adjnode],adjnode});
-                    path[adjnode]=path[node];
+                    dis[ele]=d+wt;
+                    pq.push({d+wt,ele});
+                    path[ele]=path[node];
                 }
-                else if(dis+wt==distance[adjnode])
+                else if(dis[ele]==d+wt)
                 {
-                    path[adjnode]=(path[adjnode]+path[node])%mod;
+                    path[ele]=(path[ele]+path[node])%mod;
                 }
             }
         }
-        return (path[n-1])%mod;
+        return path[n-1]%mod;
     }
 };

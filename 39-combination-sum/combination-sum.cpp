86fa867774f9a9b31c2802ef9411bc61e3bd6e05
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-09-28 11:57:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.97%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void combo(int indx, vector<int>& candidates, int target, vector<vector<int>>& result, vector<int>& ans)
+    void combo(int ind,vector<int>&candidates,int target,vector<vector<int>>&result,vector<int>&ans)
     {
-        int n = candidates.size();
-        if(indx == n)
+        int n=candidates.size();
+        if(ind==n)
         {
-            if(target == 0)
+            if(target==0)
             {
                 result.push_back(ans);
             }
             return;
         }
-        if(candidates[indx] <= target)
+        if(candidates[ind]<=target)
         {
-            ans.push_back(candidates[indx]);
-            combo(indx,candidates,target - candidates[indx],result,ans);
+            ans.push_back(candidates[ind]);
+            combo(ind,candidates,target-candidates[ind],result,ans);
             ans.pop_back();
         }
-        combo(indx + 1,candidates,target,result,ans);
+        combo(ind+1,candidates,target,result,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
         vector<vector<int>>result;
         vector<int>ans;
         combo(0,candidates,target,result,ans);

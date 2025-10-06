f5beec71ebd2ac658ae121dcdd20b7e478534060
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2025-10-07 00:26:36 +0530

    Time: 15 ms (32.21%) | Memory: 18.2 MB (37.47%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,96 +1,30 @@
 class Solution {
 public:
-    int memo(int i,int buy, int cap,vector<vector<vector<int>>>&dp,vector<int>&prices)
+    int memo(int day, int task, int cap, vector<vector<vector<int>>>& dp, vector<int>& prices)
     {
-        int n=prices.size();
-        if(i==n || cap==0)
+        int n = prices.size();
+        if(day == n || cap == 0)
         {
             return 0;
-        }
-        if(dp[i][buy][cap]!=-1)
+        }    
+        if(dp[day][task][cap] != -1)
         {
-            return dp[i][buy][cap];
+            return dp[day][task][cap]; 
         }
-        int profit=INT_MIN;
-        if(buy==1)
+        int profit = INT_MIN;
+        if(task == 0)
         {
-            profit=max(-prices[i]+memo(i+1,0,cap,dp,prices),memo(i+1,1,cap,dp,prices));
+            profit = max(-prices[day] + memo(day+1,1,cap,dp,prices), memo(day+1,0,cap,dp,prices));
         }
-        else
+        else if(task == 1)
         {
-            profit=max(prices[i]+memo(i+1,1,cap-1,dp,prices),memo(i+1,0,cap,dp,prices));
+            profit = max(prices[day] + memo(day+1,0,cap-1,dp,prices), memo(day+1,1,cap,dp,prices));
         }
-        return dp[i][buy][cap]=profit;
-    }
-    int tabu(int n,int cap,vector<vector<vector<int>>>&dp,vector<int>&prices)
-    {
-        for(int i=0;i<n;i++)//cap=0
-        {
-            for(int j=0;j<2;j++)
-            {
-                dp[i][j][0]=0;
-            }
-        }
-        for(int i=0;i<2;i++)//last day array end return nothing buy and sell 
-        {
-            for(int j=0;j<cap;j++)
-            {
-                dp[n][i][j]=0;
-            }
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<2;j++)
-            {
-                for(int k=1;k<cap;k++)
-                {
-                    int profit=INT_MIN;
-                    if(j==1)
-                    {
-                        profit=max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
-                    }
-                    else
-                    {
-                        profit=max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
-                    }
-                    dp[i][j][k]=profit;
-                }
-            }
-        }
-        return dp[0][1][cap-1];
-    }
-    //cap=3
-    int space(int n,int cap,vector<vector<int>>&dp,vector<int>&prices) //n*(2*3)
-    {
-        for(int i=n-1;i>=0;i--)
-        {
-            vector<vector<int>>temp(2,vector<int>(cap,0));
-            for(int j=0;j<2;j++)
-            {
-                for(int k=1;k<cap;k++)//k<3
-                {
-                    int profit=INT_MIN;
-                    if(j==1)
-                    {
-                        profit=max(-prices[i]+dp[0][k],dp[1][k]);
-                    }else
-                    {
-                        profit=max(prices[i]+dp[1][k-1],dp[0][k]);
-                    }
-                    temp[j][k]=profit;
-                }
-            }
-            dp=temp;
-        }
-        return dp[1][cap-1];
+        return dp[day][task][cap] = profit;
     }
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        // return memo(0,1,k,dp,prices);
-        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));//k=2
-        // return tabu(n,k+1,dp,prices);
-        vector<vector<int>>dp(2,vector<int>(k+1,0));//k=2 dp->3
-        return space(n,k+1,dp,prices);//k=3
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return memo(0,0,k,dp,prices);
     }
 };

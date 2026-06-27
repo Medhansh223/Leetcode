64b2a2a3ea425d185f0078e42182aece1b9b2189
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-06-27 20:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.03%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -32,9 +32,37 @@ public:
         int up = memo(i-1,j,dp);
         return dp[i][j] = left + up;
     }
+    int tabu(int m, int n, vector<vector<int>>& dp)
+    {
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int left = 0;
+                if(j > 0)
+                {
+                    left = dp[i][j-1];
+                }
+                int up = 0;
+                if(i > 0)
+                {
+                    up = dp[i-1][j];
+                }
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
+    }
     int uniquePaths(int m, int n) {
         // return recur(m-1,n-1);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return memo(m-1,n-1,dp);
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return memo(m-1,n-1,dp);
+        return tabu(m,n,dp);
     }
 };

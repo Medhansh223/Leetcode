08c8c88d2ed22a045b5c9e5c81ddc430a5271f7c
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-06-27 17:26:12 +0530

    Time: 3 ms (9.60%) | Memory: 9.4 MB (29.37%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int recur(int i, int j)
+    {
+        if(i == 0 && j == 0)
+        {
+            return 1;
+        }
+        if(i < 0 || j < 0)
+        {
+            return 0;
+        }
+        int left = recur(i,j-1);
+        int up = recur(i-1,j);
+        return left + up;
+    }
     int memo(int i, int j, vector<vector<int>>& dp)
     {
         if(i == 0 && j == 0)
@@ -16,38 +30,11 @@ public:
         }
         int left = memo(i,j-1,dp);
         int up = memo(i-1,j,dp);
-        dp[i][j] = left + up;
-        return dp[i][j];
-    }
-    int tabu(int m, int n, vector<vector<int>>& dp)
-    {
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i == 0 && j == 0)
-                {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                int left = 0;
-                if(j-1 >= 0)
-                {
-                    left = dp[i][j-1];
-                }
-                int up = 0;
-                if(i-1 >= 0)
-                {
-                    up = dp[i-1][j];
-                }
-                dp[i][j] = left + up;
-            }
-        }
-        return dp[m-1][n-1];
+        return dp[i][j] = left + up;
     }
     int uniquePaths(int m, int n) {
+        // return recur(m-1,n-1);
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        // return memo(m-1,n-1,dp);
-        return tabu(m,n,dp);
+        return memo(m-1,n-1,dp);
     }
 };

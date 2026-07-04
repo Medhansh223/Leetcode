cd4ee1cc7828bcf3f1d18cbf8da91e8fe6dee824
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-07-04 23:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (88.98%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,33 +11,19 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>inorder;
-        if(root==NULL)
+    void inorder(vector<int>& result, TreeNode* root)
+    {
+        if(root == NULL)
         {
-            return inorder;
+            return;
         }
-        stack<TreeNode*>st;
-        TreeNode*node=root;
-        while(true)
-        {
-            if(node!=NULL)
-            {
-                st.push(node);
-                node=node->left;
-            }
-            else
-            {
-                if(st.empty())
-                {
-                    break;
-                }
-                node=st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node=node->right;
-            }
-        } 
-        return inorder;
+        inorder(result,root->left);
+        result.push_back(root->val);
+        inorder(result,root->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>result;
+        inorder(result, root);
+        return result;
     }
 };

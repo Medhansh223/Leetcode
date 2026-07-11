413936fea69ea91742ec0e526b9028979b4a7fa7
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-07-11 15:26:55 +0530

    Time: 30 ms (34.42%) | Memory: 25 MB (9.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<int>>& visited, vector<vector<char>>& grid)
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& visited)
     {
-        int n=grid.size();
-        int m=grid[0].size();
-        visited[row][col]=1;
         queue<pair<int,int>>q;
+        int n = grid.size();
+        int m = grid[0].size();
+        visited[row][col] = 1;
         q.push({row,col});
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
+        vector<int>drow = {-1,0,+1,0};
+        vector<int>dcol = {0,+1,0,-1};
         while(!q.empty())
         {
-            int r=q.front().first;
-            int c=q.front().second;
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && visited[nrow][ncol]!=1)
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
                 {
                     q.push({nrow,ncol});
-                    visited[nrow][ncol]=1;  
+                    visited[nrow][ncol] = 1;
                 }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int>(m));
-        int islands=0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        int count = 0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1' && visited[i][j]!=1)
+                if(grid[i][j] == '1' && visited[i][j] == 0)
                 {
-                    islands++;
-                    bfs(i,j,visited,grid);   
+                    count++;
+                    bfs(i,j,grid,visited);
                 }
             }
         }
-        return islands;
+        return count;
     }
 };

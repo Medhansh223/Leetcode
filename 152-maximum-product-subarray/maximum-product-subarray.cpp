d8a63465e78918264209f42db08afabafac5e649
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-07-14 10:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.03%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int pre = 1;
-        int suff = 1;
-        int maxi = 0;
-        if(n==1)
-        {
-            return nums[0];
-        }
+        int product = 1;
+        int maxi = nums[0];
         for(int i=0;i<n;i++)
         {
-            if(pre == 0)
+            if(product == 0)
             {
-                pre = 1;
+                product = 1;
             }
-            if(suff == 0)
+            product = product * nums[i];
+            maxi = max(maxi,product);
+        }
+        product = 1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(product == 0)
             {
-                suff = 1;
+                product = 1;
             }
-            pre = pre * nums[i];
-            suff = suff * nums[n-i-1];
-            maxi = max({maxi,pre,suff});
+            product = product * nums[i];
+            maxi = max(maxi,product);
         }
         return maxi;
     }

38e69ae816ec24827634f34f76b80c56d1fb1a8f
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-07-15 13:40:21 +0530

    Time: 57 ms (7.72%) | Memory: 25.5 MB (8.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& visited)
+    void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& visited)
     {
-        queue<pair<int,int>>q;
         int n = grid.size();
         int m = grid[0].size();
         visited[row][col] = 1;
-        q.push({row,col});
         vector<int>drow = {-1,0,+1,0};
         vector<int>dcol = {0,+1,0,-1};
-        while(!q.empty())
+        for(int i=0;i<4;i++)
         {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
             {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
-                {
-                    q.push({nrow,ncol});
-                    visited[nrow][ncol] = 1;
-                }
+                dfs(nrow,ncol,grid,visited);
             }
         }
     }
@@ -38,7 +29,7 @@ public:
                 if(grid[i][j] == '1' && visited[i][j] == 0)
                 {
                     count++;
-                    bfs(i,j,grid,visited);
+                    dfs(i,j,grid,visited);
                 }
             }
         }

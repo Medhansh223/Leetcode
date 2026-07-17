7ba2aa798deb97ab6d48a1d895557ebec5801e51
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-07-17 10:17:37 +0530

    Time: 13 ms (78.42%) | Memory: 26.4 MB (27.10%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        if(n==1)
-        {
-            if(grid[0][0]==0)
-            {
-                return 1;
-            }
-            else
-            {
-                return -1;
-            }
-        }
-        if(grid[0][0]==1 || grid[n-1][n-1]==1) 
+        int n = grid.size();
+        int m = grid[0].size();
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)
         {
             return -1;
         }
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({1,{0,0}});
-        vector<vector<int>>distance(n,vector<int>(n,INT_MAX));
-        distance[0][0]=1;
-        vector<int>drow={-1, -1, -1, 0, 1, 1, 1, 0};
-        vector<int>dcol={-1, 0, 1, 1, 1, 0, -1, -1};
-        while(!pq.empty())
+        queue<pair<int,pair<int,int>>>q;
+        q.push({1,{0,0}});
+        vector<vector<int>>distance(n,vector<int>(m,INT_MAX));
+        distance[0][0] = 1;
+        vector<int>row = {-1,-1,0,+1,+1,+1,0,-1};
+        vector<int>col = {0,+1,+1,+1,0,-1,-1,-1};
+        while(!q.empty())
         {
-            int dist=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            pq.pop();
+            int dis = q.front().first;
+            int r = q.front().second.first;
+            int c = q.front().second.second;
+            q.pop();
+            if(r == n-1 && c == m-1)
+            {
+                return dis;
+            }
             for(int i=0;i<8;i++)
             {
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==0 && dist+1<distance[nrow][ncol])
+                int nrow = r + row[i];
+                int ncol = c + col[i];
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == 0 && dis + 1 < distance[nrow][ncol])
                 {
-                    distance[nrow][ncol]=dist+1;
-                    if(nrow==n-1 && ncol==n-1)
-                    {
-                        return dist+1;
-                    }
-                    pq.push({distance[nrow][ncol],{nrow,ncol}});
+                    q.push({dis+1,{nrow,ncol}});
+                    distance[nrow][ncol] = dis + 1;
                 }
             }
         }

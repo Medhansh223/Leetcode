62926947102902a8d2f5edc35ddb1c4b0ac1bdc3
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-07-19 11:44:16 +0530

    Time: 18 ms (37.92%) | Memory: 17.2 MB (37.89%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>distance(n,vector<int>(n,INT_MAX));
-        for(auto it:edges)
+        vector<vector<int>>adj(n,vector<int>(n,INT_MAX));
+        for(auto it: edges)
         {
             int u=it[0];
             int v=it[1];
             int wt=it[2];
-            distance[u][v]=wt;
-            distance[v][u]=wt;
+            adj[u][v]=wt;
+            adj[v][u]=wt;
         }
         for(int i=0;i<n;i++)
         {
-            distance[i][i]=0;
+            adj[i][i]=0;
         }
         for(int k=0;k<n;k++)
         {
@@ -20,28 +20,28 @@ public:
             {
                 for(int j=0;j<n;j++)
                 {
-                    if(distance[i][k]!=INT_MAX && distance[k][j]!=INT_MAX)
+                    if(adj[i][k]!=INT_MAX && adj[k][j]!=INT_MAX)
                     {
-                        distance[i][j]=min(distance[i][j],distance[i][k]+distance[k][j]);
+                        adj[i][j]=min(adj[i][j], adj[i][k]+adj[k][j]);
                     }
                 }
             }
         }
-        int countcity=n;
+        int citycount=n;
         int cityno=-1;
         for(int city=0;city<n;city++)
         {
             int count=0;
             for(int adjcity=0;adjcity<n;adjcity++)
             {
-                if(distance[city][adjcity]<=distanceThreshold)
+                if(adj[city][adjcity]<=distanceThreshold)
                 {
                     count++;
                 }
             }
-            if(count<=countcity)
+            if(count<=citycount)
             {
-                countcity=count;
+                citycount=count;
                 cityno=city;
             }
         }

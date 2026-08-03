8a9c87408a558946f2c855ca4e7b69a3061702a3
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-08-03 12:22:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (24.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -33,10 +33,26 @@ public:
         dp[i] = max(pick, not_pick);
         return dp[i];
     }
+    int tabu(vector<int>& dp, vector<int>& nums, int n)
+    {
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int not_pick = dp[i-1];
+            int pick = nums[i];
+            if(i >= 2)
+            {
+                pick = pick + dp[i-2];
+            }
+            dp[i] = max(pick, not_pick);
+        }
+        return dp[n-1];
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         // return recur(nums, n-1);
         vector<int>dp(n,-1);
-        return memo(dp, nums, n-1);
+        // return memo(dp, nums, n-1);
+        return tabu(dp, nums, n);
     }
 };

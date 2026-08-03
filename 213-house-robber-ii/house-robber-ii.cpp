9033ac6b531f42367b934817e61dade887d7ba34
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-08-03 12:27:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (19.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,84 +1,85 @@
 class Solution {
 public:
-    int memo(vector<int>&dp,int n,vector<int>&nums)
+    int memo(int i, vector<int>& dp, vector<int>& nums)
     {
-        if(n==0)
+        if(i == 0)
         {
-            return nums[n];
+            return nums[0];
         }
-        if(dp[n]!=-1)
+        if(dp[i] != - 1)
         {
-            return dp[n];
+            return dp[i];
         }
-        int pick=nums[n];
-        if(n-2>=0)
+        int not_pick = memo(i-1,dp,nums);
+        int pick = nums[i];
+        if(i >= 2)
         {
-            pick=pick+memo(dp,n-2,nums);
+            pick = pick + memo(i-2,dp,nums);
         }
-        int nonpick=memo(dp,n-1,nums);
-        dp[n]=max(pick,nonpick);
-        return dp[n];
+        dp[i] = max(pick,not_pick);
+        return dp[i];
     }
-    int tabu(vector<int>&dp,int n,vector<int>&nums)
+    int tabu(int n, vector<int>& dp, vector<int>& nums)
     {
-        dp[0]=nums[0];
+        dp[0] = nums[0];
         for(int i=1;i<n;i++)
         {
-            int pick=nums[i];
-            if(i-2>=0)
+            int not_pick = dp[i-1];
+            int pick = nums[i];
+            if(i >= 2)
             {
-                pick=pick+dp[i-2];
+                pick = pick + dp[i-2];
             }
-            int nonpick=dp[i-1];
-            dp[i]=max(pick,nonpick);
+            dp[i] = max(pick,not_pick);
         }
         return dp[n-1];
     }
-    int space(int n,vector<int>&nums)
+    int space(vector<int>& dp, vector<int>& nums)
     {
-        int prev1=nums[0];
-        int prev2=0;
+        int n = nums.size();
+        int prev1 = nums[0];
+        int prev2 = 0;
         int curr;
         for(int i=1;i<n;i++)
         {
-            int pick=nums[i];
-            if(i-2>=0)
+            int not_pick = prev1;
+            int pick = nums[i];
+            if(i >= 2)
             {
-                pick=pick+prev2;
+                pick = pick + prev2;
             }
-            int nonpick=prev1;
-            curr=max(pick,nonpick);
-            prev2=prev1;
-            prev1=curr;
+            curr = max(pick,not_pick);
+            prev2 = prev1;
+            prev1 = curr;
         }
         return prev1;
     }
     int robo(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         vector<int>dp(n,-1);
-        // return memo(dp,n-1,nums);
-        // return tabu(dp,n,nums);
-        return space(n,nums);
+        // return memo(n-1,dp,nums);
+        // return tabu(n,dp,nums);
+        return space(dp,nums);
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
+        int n = nums.size();
+        if(n == 1)
         {
             return nums[0];
         }
-        vector<int>left;
-        vector<int>right;
+        vector<int>not_first;
+        vector<int>not_last;
         for(int i=0;i<n;i++)
         {
-            if(i!=0)
+            if(i != 0)
             {
-                left.push_back(nums[i]);
+                not_first.push_back(nums[i]);
             }
-            if(i!=n-1)
+            if(i != n-1)
             {
-                right.push_back(nums[i]);
+                not_last.push_back(nums[i]);
             }
         }
-        return max(robo(left),robo(right));
+        return max(robo(not_first),robo(not_last));
     }
 };

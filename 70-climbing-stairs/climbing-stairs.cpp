5d4d6da0591473131466f773ee29eba9b19bcd6c
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-08-03 11:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -25,9 +25,22 @@ public:
         dp[i] = onestep+twostep;
         return dp[i];
     }
+    int tabu(vector<int>& dp, int n)
+    {
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+            int onestep = dp[i-1];
+            int twostep = dp[i-2];
+            dp[i] = onestep+twostep;
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
         // return recur(n);
         vector<int>dp(n+1,-1);
-        return memo(dp,n);
+        // return memo(dp,n);
+        return tabu(dp,n);
     }
 };

0da2560ec0a8b5f776acfed40b4f143f1262bdb2
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-08-04 16:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (77.28%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    int recur(int i, int j)
-    {
-        if(i == 0 && j == 0)
-        {
-            return 1;
-        }
-        if(i < 0 || j < 0)
-        {
-            return 0;
-        }
-        int left = recur(i,j-1);
-        int up = recur(i-1,j);
-        return left + up;
-    }
     int memo(int i, int j, vector<vector<int>>& dp)
     {
         if(i == 0 && j == 0)
@@ -30,7 +16,8 @@ public:
         }
         int left = memo(i,j-1,dp);
         int up = memo(i-1,j,dp);
-        return dp[i][j] = left + up;
+        dp[i][j] = left + up;
+        return dp[i][j];
     }
     int tabu(int m, int n, vector<vector<int>>& dp)
     {
@@ -44,12 +31,12 @@ public:
                     continue;
                 }
                 int left = 0;
-                if(j > 0)
+                if(j-1 >= 0)
                 {
                     left = dp[i][j-1];
                 }
                 int up = 0;
-                if(i > 0)
+                if(i-1 >= 0)
                 {
                     up = dp[i-1][j];
                 }
@@ -71,12 +58,12 @@ public:
                     continue;
                 }
                 int left = 0;
-                if(j > 0)
+                if(j-1 >= 0)
                 {
                     left = temp[j-1];
                 }
                 int up = 0;
-                if(i > 0)
+                if(i-1 >= 0)
                 {
                     up = dp[j];
                 }
@@ -87,10 +74,8 @@ public:
         return dp[n-1];
     }
     int uniquePaths(int m, int n) {
-        // return recur(m-1,n-1);
         // vector<vector<int>>dp(m,vector<int>(n,-1));
         // return memo(m-1,n-1,dp);
-        // vector<vector<int>>dp(m,vector<int>(n,-1));
         // return tabu(m,n,dp);
         vector<int>dp(n,-1);
         return space(m,n,dp);

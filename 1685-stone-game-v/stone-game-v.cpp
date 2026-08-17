44cb42e068090d75fd06c98c7a5df93b0643a5bb
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2026-08-17 10:44:56 +0530

    Time: 898 ms (17.46%) | Memory: 27.7 MB (33.99%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -59,6 +59,36 @@ public:
         return dp[l][r];
     }
 
+    int tabu(int n, vector<int>& prefixsum, vector<vector<int>>& dp)
+    {
+        for(int l=n-1;l>=0;l--)
+        {
+            for(int r=l+1;r<n;r++)
+            {
+                int score = 0;
+                for(int mid=l;mid<=r-1;mid++)
+                {
+                    int leftsum = prefixsum[mid] - (l-1 >=0 ? prefixsum[l-1] : 0); 
+                    int rightsum = prefixsum[r] - prefixsum[mid];
+                    if(leftsum < rightsum)
+                    {
+                        score = max(score, leftsum + dp[l][mid]); 
+                    }
+                    else if(leftsum > rightsum)
+                    {
+                        score = max(score, rightsum + dp[mid+1][r]);
+                    }
+                    else
+                    {
+                        score = max({score, leftsum + dp[l][mid], rightsum + dp[mid+1][r]});
+                    }
+                }
+                dp[l][r] = score;
+            }
+        }
+        return dp[0][n-1];
+    }
+
     int stoneGameV(vector<int>& stoneValue) {
         int n = stoneValue.size();
         vector<int>prefixsum(n+1,0);
@@ -68,7 +98,9 @@ public:
             prefixsum[i] = stoneValue[i] + prefixsum[i-1];
         }
         // return recur(0,n-1,prefixsum);
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return memo(0,n-1,prefixsum,dp);
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return memo(0,n-1,prefixsum,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        return tabu(n,prefixsum,dp);
     }
 };
